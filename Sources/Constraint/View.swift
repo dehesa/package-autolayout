e73464d9e334070c7bdffa59f3a68446d7201be4
Marcos Sánchez-Dehesa <san.dehesa@gmail.com>
2025-07-26 11:05:51 +0200

    Constraint addSubview to support subclasses

--- Sources/Constraint/View.swift
+++ Sources/Constraint/View.swift
@@ -23,14 +23,24 @@ public extension LayoutView {
     self.addSubview(view)
     constraintGenerator(view)
   }
+}
 
+public extension NSObjectProtocol where Self: LayoutView {
   /// Prepares the given parameter view to work in autolayout, adds it to the receiver view hierarchy, and executes (immediately) the given closure.
   /// - parameter view: The view to be added to the receiver view hierarchy.
   /// - parameter constraintGenerator: Closure typically used to define constraint for the receiving view.
   /// - parameter superview: The view receiving the `addSubview(_:)` method call.
-  @inlinable func addSubview<V>(_ view: V, _ constraintGenerator: (_ superview: Self, _ view: V) -> Void) where V: LayoutView {
+  @inlinable @MainActor func addSubview<V>(_ view: V, _ constraintGenerator: (_ superview: Self, _ view: V) -> Void) where V: LayoutView {
     view.translatesAutoresizingMaskIntoConstraints = false
     self.addSubview(view)
-    constraintGenerator(unsafeDowncast(self, to: Self.self), view)
+    constraintGenerator(self, view)
   }
+
+  #if os(macOS)
+  @inlinable @MainActor func addSubview<V1, V2>(_ view: V1, positioned place: NSWindow.OrderingMode, relativeTo otherView: V2, _ constraintGenerator: (_ superview: Self, _ view: V1, _ otherView: V2) -> Void) where V1: LayoutView, V2: LayoutView {
+    view.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(view, positioned: place, relativeTo: otherView)
+    constraintGenerator(self, view, otherView)
+  }
+  #endif
 }
